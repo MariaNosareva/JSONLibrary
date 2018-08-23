3a5b3d5d2b66ec7bea408c76b0918c993b56ea5a
Maria Nosareva <mashunya_19-97@mail.ru>
2018-08-23 14:33:03 +0300

    main skeleton

---
+++ JSONLibrary/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace JSONLibrary {
+    public static class ObjectTransform {
+        public static String ToJson(this Object obj) {
+            return "";
+        }
+
+        public static void ToJsonFile(this Object obj, string fileName) { }
+    }
+
+    public static class StringTransform {
+        public static T FromJson<T>(this String str) where T: new() {
+            return new T();
+        }
+    }
+}
