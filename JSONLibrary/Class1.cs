bdffb46633e73deef6cc5b6f531a01ecf9093d2c
Maria Nosareva <mashunya_19-97@mail.ru>
2018-08-23 16:56:18 +0300

    to json string

--- JSONLibrary/Class1.cs
+++ JSONLibrary/Class1.cs
@@ -1,9 +1,65 @@
 ﻿using System;
+using System.Collections;
+using System.Reflection;
+using System.Text;
 
 namespace JSONLibrary {
     public static class ObjectTransform {
+
+        public static StringBuilder valueToString(this Object value, StringBuilder valueResult) {
+            switch (value.GetType().Name) {
+                case "String":
+                    valueResult.Append("\"" + value + "\"");
+                    break;
+                case "ArrayList":
+                    valueResult.Append("[");
+                    foreach (var element in (ArrayList) value) {
+                        valueResult = element.valueToString(valueResult);
+                        valueResult.Append(", ");
+                    }
+
+                    valueResult.Remove(valueResult.Length - 2, 2).Append("]");
+                    break;
+                case "Int32":
+                    valueResult.Append(value);
+                    break;
+                case "Single":
+                    valueResult.Append(value);
+                    break;
+                case "Double":
+                    valueResult.Append(value);
+                    break;
+                case "Boolean":
+                    valueResult.Append(value.ToString().ToLower());
+                    break;
+            }
+
+            return valueResult;
+        }
+
         public static String ToJson(this Object obj) {
-            return "";
+
+            var jsonBuilder = new StringBuilder("{");
+            var type = obj.GetType();
+            
+            foreach (var property in type.GetProperties()) {
+                jsonBuilder.Append("\"" + property.Name + "\":");
+
+                var value = property.GetValue(obj, new object[] { });
+                var valueResult = new StringBuilder();
+                if (value == null) {
+                    valueResult.Append("null");
+                }
+                else {
+                    valueResult = value.valueToString(valueResult);
+                }
+                
+                jsonBuilder.Append(valueResult);
+                jsonBuilder.Append(", ");
+            }
+            
+            var str = jsonBuilder.Remove(jsonBuilder.Length-2, 2).Append("}").ToString();
+            return str;
         }
 
         public static void ToJsonFile(this Object obj, string fileName) { }
